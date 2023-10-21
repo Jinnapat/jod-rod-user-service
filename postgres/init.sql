44a76672d5400349f88a5e0f0676f280dc53d7a3
Jinnapat Yana <jaja_yana@hotmail.com>
2023-10-21 23:36:33 +0700

    Add is_admin column

--- postgres/init.sql
+++ postgres/init.sql
@@ -2,5 +2,8 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     email TEXT UNIQUE NOT NULL,
     username TEXT UNIQUE NOT NULL,
-    password_hash TEXT NOT NULL
+    password_hash TEXT NOT NULL,
+    is_admin BOOLEAN NOT NULL DEFAULT FALSE
 );
+INSERT INTO users (email, username, password_hash, is_admin)
+VALUES ('admin@jodrod.com', 'admin', '$2b$12$r74rI/6ukODKQrZ47texVuAxvp6aFenscpbOHKEoNGdGo96w3G8.W', TRUE);
