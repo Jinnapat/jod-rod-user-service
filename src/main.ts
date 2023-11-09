d11f2a8e4e00fcfa654ae0818575c8bf6673bc32
Palm394 <palm.nichapat@gmail.com>
2023-11-09 14:46:32 +0700

    service discovery

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { registerWithEureka } from './helper/eureka-helper'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  registerWithEureka('USER-SERVICE', 3000)
   await app.listen(3000);
 }
 bootstrap();
